e24ffb149b94b527b22e7646bb95768ce7c74b12
markeetagee <mtagaloguin@nerubia.com>
2022-09-28 18:27:33 +0800

    add: register user function(my original code version)

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,14 +1,16 @@
 const router = require('express').Router();
 const Joi = require('@hapi/joi');
+const bcrypt = require("bcrypt");
 
-const db = require('../db/connection');
-const users = db.get('dotenv');  
+const { pool } = require('../db/connection');
+//const users = db.get('dotenv');  
 
 const registerSchema = Joi.object().keys({
     firstName: Joi.string().alphanum().min(3).max(30).required(),
     lastName: Joi.string().alphanum().min(3).max(30).required(),
     phoneNum: Joi.string().alphanum().min(10).max(11).required(),
     password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/).required(),
+    password2: Joi.any().valid(Joi.ref('password')).required(),
     email: Joi.string().email({ minDomainSegments: 2 }),
 })
 
@@ -16,32 +18,68 @@ const registerSchema = Joi.object().keys({
 router.get('/register', (req, res) =>{
     res.send('Welcome to the Register Route')
 });
-
-router.post('/register', (req, res, next) =>{
-    //res.json(req.body)
-    
-    const result = registerSchema.validate(req.body);
-    if(!result.error){
-        pool.findOne({
-            email: req.body.email,
-        }).then(user =>{
-            console.log(user)
-            if(user !== null){
-                console.log('email already exists, please choose another..');
-                const error = new Error ('email already exists, please choose another one..');
-                next(error);
-        }else{
-            bcrypt.hash(req.body.password.trim(), saltRounds)
-            .then(hashedPassword => {
-                console.log(hashedPassword)
-            })
+  
+router.post('/register', async (req, res) => {
+        const errors = registerSchema.validate(req.body);
+        const {firstName, lastName, email, phoneNum, password, password2} = req.body;
+     
+     
+     if (errors.length > 0) {
+         res.render('register', { 
+             errors,
+             firstName,
+             lastName,
+             email,
+             phoneNum,
+             password,
+             password2 
+         
+         });
+     }else{
+        //validation passed
+     
+        let hashedPassword = await bcrypt.hash(password, 10);
+        console.log(hashedPassword);
+     
         
-    }
-})
-    }else{
-        res.json(result.error);
-    }
-})   
+        pool.query(
+            `SELECT * FROM users WHERE email = $1 `, [email], (err, results)=>{
+                if (err){ 
+            res.json(results.err); 
+        }
+      //  console.log("reaches here"); -> Just to check if the code is working.
+        console.log(results.rows);
+     
+        if(results.rows.length > 0){
+            errors.push({message: "Email already registered!"});
+            res.render("register", {errors});
+     
+        }else{ // will save the data into our database.
+            pool.query(
+                `INSERT INTO users (fname, lname, email, phonenum, password)
+                    VALUES  ($1, $2, $3, $4, $5)
+                    RETURNING id, password`, [fname, lname, email, phonenum, hashedPassword],
+                    (err, results) => {
+                        if (err){
+                            throw err
+                        }
+                        console.log(results.rows);
+                        req.flash('success_msg', "You are now registered! Please log-in");
+                        res.redirect("/users/login");
+                    }
+            );
+        }
+     
+        
+                });
+     
+     
+     }
+     
+     
+     });    
+
+
 
 router.get('/login', (req, res) =>{
     res.send('Welcome to the Login Route')
